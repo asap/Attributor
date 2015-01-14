1a2ea84862bd9bbe3c023fd4bf521bd2c26e8634
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2015-01-13 21:44:48 -0500

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Alexander Sapountzis on 1/13/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
